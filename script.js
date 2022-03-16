7d1f61e932ed6063aa4e83e59a809fc9e680106b
Arbaz Yousaf <arbaz.k480@yahoo.com>
2022-03-16 16:13:43 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,10 +4,21 @@ const advice = document.querySelector('.advice');
 
 newAdviceBtn.addEventListener('click', newAdvice);
 
-async function newAdvice() {
-  const STREAM = await fetch('https://api.adviceslip.com/advice');
-  const response = await STREAM.json();
 
-  id.innerHTML = response['slip']['id']
-  advice.innerHTML = response['slip']['advice']
+async function newAdvice() {
+  // animate newAdviceBtn and display the message while data is being fetched
+  newAdviceBtn.classList.add('animate');
+  advice.innerHTML = 'Please wait!';
+  
+  try {
+    const STREAM = await fetch('https://api.adviceslip.com/advice');
+    const response = await STREAM.json();
+    
+    newAdviceBtn.classList.remove('animate');
+    id.innerHTML = response['slip']['id']
+    advice.innerHTML = response['slip']['advice']
+  } catch(err) {
+    id.innerHTML = 'error';
+    advice.innerHTML = err;
+  }
 }
